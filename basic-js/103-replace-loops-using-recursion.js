297b3771fa9b64f3f0b8765ddb02b4e92c8d0284
Atharva Pardeshi <atharva.exe@gmail.com>
2022-06-02 17:30:29 +0530

    feat: add file

---
+++ basic-js/103-replace-loops-using-recursion.js
@@ -0,0 +1,11 @@
+function sum(arr, n) {
+  // Only change code below this line
+  if(n <= 0) {
+    return 0;
+  } else {
+    return sum(arr, n - 1) + arr[n - 1];
+  }
+  // Only change code above this line
+}
+
+// Honestly, this is a little difficult for me to understand. Will have to do a little more reading.
