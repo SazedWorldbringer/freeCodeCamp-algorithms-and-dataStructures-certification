ac22363d5baa1910cdc80451a0ad76b6798a0050
Atharva Pardeshi <atharva.exe@gmail.com>
2022-07-08 22:31:05 +0530

    feat: add file

---
+++ functional-programming/19-split-a-string-into-an-array-using-the-split-method.js
@@ -0,0 +1,8 @@
+function splitify(str) {
+  // Only change code below this line
+  const splitStr = str.split(/\W/);
+  return splitStr;
+  // Only change code above this line
+}
+
+splitify("Hello World,I-am code");
